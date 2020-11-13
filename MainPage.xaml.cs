8fe2a7676f112b564c6308fd42abc53aa485f84f
Tamino1762 <40547572+Tamino1762@users.noreply.github.com>
2020-11-13 13:40:46 -0600

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QuizApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //array of questions
+        //array of answers
+        //answer logic
+
+        int score = 0;
+        string question = "";
+        //true button
+        void Handle_True(object sender, System.EventArgs e)
+        {
+            score += 1;
+            question += Questions.Questioner(question);
+            theQuestion.Text = question;
+            //when clicked, keep answer, go to next question
+
+        }
+        //false button
+        void Handle_False(object sender, System.EventArgs e)
+        {
+            //when clicked, keep answer, go to next question
+        }
+
+        void Handle_Start(object sender, System.EventArgs e)
+        {
+            start.IsVisible = false;
+            trueButton.IsVisible = true;
+            falseButton.IsVisible = true;
+            question += Questions.Questioner(question);
+            theQuestion.Text = question;
+            
+        }
+        //question place - Questions.Questioner
+        //theQuestions += Questions.Questioner(*local variable here*)
+        //theQuestion.Text = theQuestions;
+
+    }
+}
---
+++ Questions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Schema;
+
+namespace QuizApp
+{
+    public class Questions
+    {
+        private static string[] questions = new string[] {
+                "There are 3 sharps in the key of A Major.", "An F7 chord includes a C", "A minor chord includes a lowered 3rd.",
+                "The key of C minor has 3 flats","A Concerto consists of 3 movements."};
+
+
+        public Questions()
+        {
+
+        }
+
+        public static string Questioner(string question)
+        {
+            int score = 0;
+            string q = "?";
+
+            //get question - loop through questions, get answer
+            for (int i = 0; i < questions.Length; i++)
+            {
+                
+                q = questions[i];
+                break;
+               
+            }
+            return q + 1;
+                //check answer
+                //if answer is correct, add to score
+                //when questions are done, display score
+        }
+
+
+
+        //public static string Questioner(string question)
+        //{
+        //    bool result;
+        //    for (int i = 0; i < questions.Length; i++)
+        //    {
+        //        if (questions[i].Equals(questions))
+        //        {
+        //            result = answers[i];
+        //            break;
+        //        }
+        //    }
+        //    return;
+        //}
+    }
+}
